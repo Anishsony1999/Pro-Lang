07110fcdae7d505e042b17013cf4daf02223a869
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-04-28 20:52:30 +0530

    oops adding

--- javalang/OOps.java
+++ javalang/OOps.java
@@ -40,7 +40,49 @@ public class OOps {
     // 6) Encapsulation
 
     public static void main(String[] args) {
-           
+        // create object of String
+
+        String str = new String("Hai bro");
+
+        // //creating object of Home class
+        // Home abiBhavan = new Home();
+
+        // abiBhavan.room = 4;
+        // abiBhavan.color = "pistaGreen";
+        // abiBhavan.lights = "white";
+        // abiBhavan.floor = 3;
+
+        // System.out.println(abiBhavan.room);
+
+
+        // Home jithuBhavan = new Home();
+        // jithuBhavan.room = 3;
+        // jithuBhavan.color = "white";
+        // jithuBhavan.lights = "white";
+        // jithuBhavan.floor = 3;
+
+        // System.out.println(jithuBhavan.room);
+
+        // Home sandraBhavan = new Home();
+        // sandraBhavan.room = 4;
+        // sandraBhavan.color = "white";
+        // sandraBhavan.lights = "white";
+        // sandraBhavan.floor = 3;
+
+        // System.out.println(sandraBhavan.room);
+
+        // Creating Home Object using parameterized constructor
+
+        Home abiBhavan = new Home(4,"pistaGreen","white",3);
+        Home jithuBhavan = new Home(3,"white","white",3);
+        Home sandraBhavan = new Home(4,"white","white",3);
+
+        System.out.println(abiBhavan.room);
+        System.out.println(jithuBhavan.room);
+        System.out.println(sandraBhavan.room);
+
+        
+        
     }
 
 }
@@ -52,6 +94,8 @@ public class OOps {
 // }
 
 
+// this is a data type (Home)
+
 class Home{
     int room ;
     String color;
@@ -60,6 +104,17 @@ class Home{
 
     // constructor
     // method
+
+    Home(){
+        System.out.println("Home is created");
+    }
+
+    Home(int room,String color,String lights,int floor){
+        this.room = room;
+        this.color = color;
+        this.lights = lights;
+        this.floor = floor;
+    }
 }
 
 // Class :-
@@ -71,3 +126,18 @@ class Home{
 
 // 1) Object is an instance of a class.
 // 2) Object is a real world entity.
+
+// Constructor :-
+// 1) Constructor is a special method that is used to initialize the object.
+// 2) Constructor is a method that is called when an object is created.
+// 3) Constructor name is same as class name.
+// 4) Constructor doesn't have any return type.
+
+    // 1) Default Constructor :-
+        // Default Constructor is a constructor that is called when an object is created.
+        // default ""    is created by java compiler.
+        // if we don't create any constructor then java compiler will create a default constructor.
+        // if we declare a class then java compiler will create a default constructor.
+
+
+    // 2) Parameterized Constructor :-
