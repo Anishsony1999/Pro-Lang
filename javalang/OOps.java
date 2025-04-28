b7b4bd80819945e078eb819c23d3c6751c8ddf30
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-04-28 20:10:05 +0530

    oops

---
+++ javalang/OOps.java
@@ -0,0 +1,73 @@
+package javalang;
+
+public class OOps {
+    // OOps  - Object Oriented Programming
+
+    // What is OOps ?
+    // this is a programming paradigm that is based on the concept of objects.
+
+
+    // Createing real Home 
+    // we need a plan - engineer - blueprint
+    // we need a builder - constructor team
+    // we need a material - object
+    
+    
+    // Jithu - home
+    // jithu  will the same engineer - blueprint
+    // jithu  will the same builder - constructor team
+    // jithu  will the same material - object
+
+    // Sandra - home
+    // sandra  will the same engineer - blueprint
+    // sandra  will the same builder - constructor team
+    // sandra  will the same material - object
+
+    // oops :-
+    // 1) Class - blueprint 
+    // 2) Object - home
+    // 3) Constructor - builder team
+
+
+    // oops :
+    // 1) Class
+    // 2) Object
+
+    // pillar of oops :
+    // 3) Inheritance
+    // 4) Polymorphism
+    // 5) Abstraction
+    // 6) Encapsulation
+
+    public static void main(String[] args) {
+           
+    }
+
+}
+
+
+// class syntax :
+// class ClassName{
+//     // code
+// }
+
+
+class Home{
+    int room ;
+    String color;
+    String lights;
+    int floor;
+
+    // constructor
+    // method
+}
+
+// Class :-
+// 1) Class is a blueprint of an object.
+// 2) class have meathods, properties , constructors, variables, etc.
+
+
+// Object :-
+
+// 1) Object is an instance of a class.
+// 2) Object is a real world entity.
