9df2397353292549a6b189aed234b1783b79b3ec
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-05-22 10:13:18 +0530

    adding

--- JS/index.js
+++ JS/index.js
@@ -49,9 +49,92 @@ add();
 // 4. Undefined 
 // 5. Null - empty
 
-num1 = prompt("Enter a number ");
-num2 = prompt("Enter a number ");
+// num1 = prompt("Enter a number ");
+// num2 = prompt("Enter a number ");
 
-console.log(num1 + num2); // string concatenation 2020
-console.log(parseInt(num1) + parseInt(num2)); // parseInt() converts string to integer 20 + 20
+// console.log(num1 + num2); // string concatenation 2020
+// console.log(parseInt(num1) + parseInt(num2)); // parseInt() converts string to integer 20 + 20
+
+
+var str = "100";
+
+var num = Number(str); // 100 int
+
+const date = new Date();
+
+// console.log(date);
+
+// boolean val
+
+var ans = Number(true); // true 1 | false 0
+
+
+var ans = true ;
+var printing =  ans ? 1 : 0 
+
+
+// String handling
+
+let uName = "Anish";
+alert(uName.length);
+
+fname = "anish";
+lname = "sony";
+
+fullName = fname.concat(" ",lname);
+
+alert(fullName)
+
+str = "welcome to tvm";
+
+welcome  = str.replace("tvm" , "SCOPE INDIA");
+
+alert(welcome);
+
+str = "   i am from tvm   ";
+
+out1 = str.length;
+alert("str real length : " + out1); // 19
+
+out = str.trim();
+
+alert("after trim : " +  out.length); // 13
+alert(out)
+
+
+str.toUpperCase() // change to upper case
+str.toLowerCase() // change to lower case
+
+// Math functions
+
+x = 20.2
+
+Math.round(x); // 20.2 -> 20
+Math.floor(x); // 20.2 -> 20
+Math.ceil(x); // 20.2 -> 21
+
+Math.min(10,20); // 10
+Math.max(10,20); // 20
+Math.pow(5,2); // 5**2
+Math.abs(-18); // 18
+Math.sqrt(25); // 5
+
+
+// Date and Time
+
+
+// const date = new Date();
+
+alert(date);
+
+
+date.getDate(); // date
+date.getDay();
+date.getFullYear();
+date.getSeconds();
+date.getHours();
+date.getTime();
+
+
+
 
