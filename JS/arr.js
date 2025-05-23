3df4d1724ca9a50a9c9da60c24752c4a1844dc1b
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-05-23 11:01:42 +0530

    23 - 5

--- JS/arr.js
+++ JS/arr.js
@@ -31,8 +31,47 @@ document.writeln("<h1>"+ names[2] + "</h1>");
 //two-dimenstional arrays
 
 var furniture = [
-    ["red","green","blue"],
+    ["red","green",["arra","afyfa",["anish"]]],
     ["chair","table","stool"]
 ]
 
-document.writeln(furniture[1][1])
+document.writeln(furniture[0][2][2][0])
+
+// JSON - javascript object notation
+
+var person = [
+    "anish", // 0
+    "sony", // 1
+    "tvm"  // 2
+]
+
+var person1 = {
+    name : "anish", // 0 - name
+    age : 24,
+    location : "tvm"
+}
+
+document.writeln(person[1]);
+document.writeln(person1["age"]);
+document.writeln(person1.age);
+
+
+var kids = [
+    {
+        name : "anish",
+        age : 24,
+        location : "tvm"
+    },
+    {
+        name : "sooraj",
+        age : 24,
+        location : "tvm"
+    },
+    {
+        name : "merin",
+        age : 24,
+        location : "tvm"
+    }
+]
+
+document.writeln(kids[2]["name"])
