c3d7917a8ae00e6f439bc253a7a932ced7b831d7
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-06-03 14:04:04 +0530

    adding nre html

---
+++ html/htmls/html.js
@@ -0,0 +1,26 @@
+// HTML - HyperText Markup Language
+
+// Abbreviations:-
+// HTML - HyperText Markup Language
+// CSS - Cascading Style Sheets
+// JS - JavaScript
+// HTTP - HyperText Transfer Protocol
+// URL - Uniform Resource Locator
+// SSl - Secure Sockets Layer
+// Encypted - converted to encrypted form
+// HTTPS - HyperText Transfer Protocol Secure
+// FTPS - File Transfer Protocol Secure 
+// IIS - Internet Information Services
+// Protocal - set of rules for communication between two computers
+// IP - Internet Protocol
+// DNS - Domain Name System
+// XML - Extensible Markup Language
+// JSON - JavaScript Object Notation
+// AJAX - Asynchronous JavaScript and XML
+// API - Application Programming Interface
+// YAML - Yet Another Markup Language
+
+// Server - computer that provides services to other computers
+// Client - computer that requests services from other computers
+// Browser - software that displays web pages
+
