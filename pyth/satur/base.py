9b61ac123e79943aaa7a30d3e805c13be764a4c4
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-06-14 10:31:51 +0530

    adding py

---
+++ pyth/satur/base.py
@@ -0,0 +1,48 @@
+# programing 2 type type language
+# 1, lowlevel language - 0101010
+# 2, high level language - English
+
+
+# highlevel language  to lowlwevel language - complier and interpreter
+
+# complier vs interpreter
+# complier - compile the entire code and generate the output
+# interpreter - line by line translation of the code and generate the output
+
+# python is a highlevel language
+# python is a interpreted language
+
+# python is a dynamically typed language
+# python is a statically typed language
+
+
+# variable
+# variable is a name that is used to store a value
+# variable is a reference to a value
+
+# comments
+# comments are used to explain the code
+# interpreter will ignore the comments
+
+# in python we must keep space 
+# python is a case sensitive language
+
+
+x = 10 # x is the name of the variable and 10 is the value
+name = "Anish"
+
+print(x) # 10
+print(name) # Anish
+
+# data types
+# int, float, string, boolean:
+
+
+x = 10 # x is integer data type
+y = 10.5 # y is float data type
+name = "Anish" # name is string data type 
+is_true = False # is_true is boolean data type
+
+
+
+
