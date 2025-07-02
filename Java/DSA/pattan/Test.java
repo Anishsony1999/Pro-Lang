009c9ada250f84beed161515410667c64122b1e9
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-02 09:15:59 +0530

    adding

---
+++ Java/DSA/pattan/Test.java
@@ -0,0 +1,38 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test{
+
+    // int i;
+
+    public static void main(String[] args) {
+        
+        // Test t = new Test();
+
+        // for(t.i=0;t.i<10;t.i++){
+        //     System.out.println(t.i);
+        // }
+
+        for(int i=1; i<=10;i++){
+            System.out.println(i);
+        }
+
+
+        int j=0;
+
+        while(j<10){
+            j++;
+        }
+
+        // for()
+
+        List<String> list = new ArrayList<>();
+
+        // list.stream().forEach();
+
+        for(String s : list){
+            System.out.println(s);
+        }
+        
+    }
+}
