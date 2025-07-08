d4500e92423155322ac2f07e329613baff04ddc3
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-08 18:40:20 +0530

    adidng

---
+++ html/js/ava.js
@@ -0,0 +1,31 @@
+
+// window.alert("Hello World")
+
+
+// userInput = window.confirm("Are You Sure? ") // confirm function return boolean value(ture or false)
+
+// alert(userInput)
+
+// user_name = prompt("Enter Your Name");
+
+// ans = confirm("Aru Your Sure To Display Your Name? ");
+
+// if (ans ==true) document.writeln("<h1> Welcome "+ user_name +"</h1>")
+// else document.writeln("<h1> Welcome Guest </h1>")
+
+// condition ? true : false;
+// ans ? document.writeln("<h1> Welcome "+ user_name +"</h1>") : document.writeln("<h1> Welcome Guest </h1>");
+
+// spread operator
+// ...
+
+let arr1 = [1,2,3,4]
+let arr2 = [5,6,...arr1,7,8]
+console.log(arr2)
+
+// str concation
+
+fname = "avanthika"
+lname = "R"
+console.log( fname + " " + lname)
+console.log(`${fname} ${lname}`)
---
+++ pyth/even6-7/index.py
@@ -0,0 +1,25 @@
+
+# variables
+
+x = 4 
+y = 4.4
+name = "Anish"
+is_bool = True 
+
+print(type(is_bool))
+
+# Conditions 
+
+# if condition 
+
+# Syntax 
+# if condition:
+
+x = 10
+y = 15
+
+if x > y :
+    print(" x is greater than y ")
+
+if x < y :
+    print(" x is less than y")
