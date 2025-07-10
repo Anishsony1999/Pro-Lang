a29480cebbcd70fa5bd5a8a10e0ad946d494b41d
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-10 10:22:14 +0530

    pali in for

--- pyth/py9-11/task.py
+++ pyth/py9-11/task.py
@@ -1,16 +1,37 @@
-# String rev
 
-# u = input("Enter Any Think :\n")
 u = "Hello World"
 
-# for i in range(len(u)-1,-1,-1):
-#     print(u[i],end="")
-#     # print(i)
+ans =""
 
-# ans = ""
+arr = u.split()
 
-for i in u:
-    print(i)
-    # ans = i + ans
+for i in arr:
+    rev =" "
+    for j in i:
+        rev = j + rev
+    ans += rev
 
-# print(ans)
+print(ans)
+
+list1 = [2,4,5,7,89,0,212,3,42,5]
+
+count = 0
+
+for i in range(len(list1)):
+    if list1[i] %2 ==0 and list1[i] !=0 :
+        count = count+1
+
+print(count)
+
+str = "Amma"
+ans =""
+for i in str:
+    if i.isalpha():
+        ans = ans + i
+rev=""
+
+for i in ans:
+    rev = i + rev
+
+if rev == ans : print("this is pali")
+else : print("is not a pali")
