e0e27f6a6ffbb3eb8c37fd1d7fe28073fc9deabc
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-10 09:46:23 +0530

    Task String

---
+++ pyth/py9-11/task.py
@@ -0,0 +1,16 @@
+# String rev
+
+# u = input("Enter Any Think :\n")
+u = "Hello World"
+
+# for i in range(len(u)-1,-1,-1):
+#     print(u[i],end="")
+#     # print(i)
+
+# ans = ""
+
+for i in u:
+    print(i)
+    # ans = i + ans
+
+# print(ans)
