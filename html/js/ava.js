acfef00594c6a2172f9f971cdbc5f6447e2e7c2e
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-14 17:59:53 +0530

    ava adding

--- html/js/ava.js
+++ html/js/ava.js
@@ -124,3 +124,92 @@ Math.abs(-4) // 4
 
 console.log(Math.round(Math.random() * 10))
 
+date =  new Date(); // creating new date object
+console.log(date);
+console.log(date.getTime())
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getHours())
+console.log(date.getMinutes())
+console.log(date.getSeconds())
+console.log(date.getMilliseconds())
+
+let timeFormation = {
+    year : "numeric",
+    month : "long",
+    day : "numeric",
+}
+
+console.log(date.toLocaleString("en-GB",timeFormation))
+
+// array 
+
+arr = []
+arr2 = new Array();
+
+x = [1,"Anish",true,10.0]
+ 
+console.log(x[1])
+
+// two dimensional array
+
+x = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+console.log(x[1][1]);
+
+// JSON - JavaScript Object Notation
+
+x = ["Anish",24,"Tvm"]
+
+user ={
+    'name' : 'Anish',
+    'age' : 24,
+    'city' : 'Tvm'
+}
+
+console.log(user['name'])
+console.log(user.name)
+console.log(x[0])
+
+JSON.stringify(user) // convert object to string
+//JSON.parse(user) // convert string to object
+
+// Array Handling Methods
+
+names = ["Anish","Avanthika"]
+
+// push()
+names.push("Sony") // adding element at the end
+names.unshift("Ava") // adding element at the beginning
+
+console.log(names)
+
+names.pop() // removing element from the end
+console.log(names)
+
+names.shift() // removing element from the beginning
+console.log(names)
+
+// names.splice(1,1) // removing element from the index 1
+
+console.log(names)
+
+names.reverse() 
+
+console.log(names)
+
+names.sort()
+
+console.log(names)
+
+// toSrting()
+// Array to String
+
+console.log(names.toString())
+
+
