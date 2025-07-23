a4710c1fa9526847d83459ba3a3062ff472de438
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-23 16:00:35 +0530

    java

--- javalang/dona/Oops.java
+++ javalang/dona/Oops.java
@@ -13,35 +13,31 @@ public class Oops {
     // 4. abstraction
 
     // class -> class is the blueprint of the object
-    // object -> object is the instance of the class
+    // object -> object is the instance of the class, object are create in runtime
+
+
+    // constructor -> meathod -> access modifiers
+
+    // access modifiers -> public , private , protected , default
+
+    // public -> can be accessed from anywhere (in same package and outside package)
+    // private -> can be accessed only in the same class
+    // protected -> can be accessed in the same package and outside package (in sub class)
+    // default -> can be accessed in the same package (in same class and sub class)
+
 
     public static void main(String[] args) {
-        // int x;
-        // Home bh = new Home();
-        // Home sony = new Home();
-        // // System.out.println(x); 
-
-        // System.out.println(bh.color);
-        // System.out.println(sony.color);
-        int arr[]= {1,3,5,2,4,9};
-        int k=0;
-        for (int i=0; i <arr.length; i++){
-            k=arr[i];
-            System.out.println("loop i : " + k);
-            for (int j= i+1; j<arr.length; j++)
-                k = k+ arr[j];
-                System.out.println(k);
-                if (k == 6) {
-                    System.out.println(i + ", " + k);
-                }
-            
-        }
+    
+        Home bh = new Home();
+        
     }
 }
 class Home{
     String color;
     int room ;
     int chairs;
+
+    
 }
 
 
