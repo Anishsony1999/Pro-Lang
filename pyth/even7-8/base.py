183bb0103f30ca7ee0ba14cdfdb6efb136bb7bd9
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-24 09:22:11 +0530

    Adding base.py

---
+++ pyth/even7-8/base.py
@@ -0,0 +1,23 @@
+
+
+# programing language in two types;
+
+
+# 1. compiled language - convert into machine language - binary code
+# 2. interpreted language - line by line execution
+
+# compiler - c,c++,java
+# interpreter - python, javascript, php, ruby, bash, shell
+
+# python is interpreted language
+
+# 9442016203 -call
+# 9080264554 - message
+
+# comments - single line - #
+# multiline comments - ''' ''' or """ """
+
+
+print("Hello World")
+
+# ctrl + `
