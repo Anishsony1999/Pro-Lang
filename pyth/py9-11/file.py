a9546441dad79423659aea1b5b31b0cad98fc1be
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-07-29 12:31:17 +0530

    adding

--- pyth/py9-11/file.py
+++ pyth/py9-11/file.py
@@ -1,48 +1,59 @@
 
-# file reading and writing in python
+# # file reading and writing in python
 
-# 1, file reading  - open()
+# # 1, file reading  - open()
 
 
 path = "C:/Users/HP/OneDrive/Desktop/Lang/pyth/test.txt"
 
 
-# try:
-#     file = open(path,"r")
-#     print(file.read())
-#     file.close()
-# except Exception as e:
-#     print(e)
+# # try:
+# #     file = open(path,"r")
+# #     print(file.read())
+# #     file.close()
+# # except Exception as e:
+# #     print(e)
 
 
-# r - read
-# w - write
-# rb - binary
-# wb - binary
-# a - append
-# r+ - read and write
-data = []
+# # r - read
+# # w - write
+# # rb - binary
+# # wb - binary
+# # a - append
+# # r+ - read and write
+# data = []
+
+# with open(path,'r') as file:
+#     data = file.read()
+#     print(data)
+
+# print("******************")
+
+# with open(path,'r') as file:
+#     line1 = file.readline()
+#     line2 = file.readline()
+
+#     print(line1,line2)
+
+# with open(path,'r') as file:
+#     data = file.readlines()
+#     print(data)
+
+# with open(path,'w') as file:
+#     file.write("This is new String1")
+
+# with open(path,'a') as file:
+#     for i in data:
+
+#         file.write(i)
+
+users = dict()
 
 with open(path,'r') as file:
-    data = file.read()
-    print(data)
 
-print("******************")
+    x = file.readlines()
+    for i in x:
+        user = i.split(":")
+        users[user[0].strip()] = user[1].replace("\n","").strip()
 
-with open(path,'r') as file:
-    line1 = file.readline()
-    line2 = file.readline()
-
-    print(line1,line2)
-
-with open(path,'r') as file:
-    data = file.readlines()
-    print(data)
-
-with open(path,'w') as file:
-    file.write("This is new String1")
-
-with open(path,'a') as file:
-    for i in data:
-
-        file.write(i)
+print(users)
