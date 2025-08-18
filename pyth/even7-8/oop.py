e514f6c6b8155deef30016f0119ec17214222721
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-08-18 19:59:11 +0530

    adding

--- pyth/even7-8/oop.py
+++ pyth/even7-8/oop.py
@@ -21,3 +21,48 @@ home1 = Home()
 home1.fan_on()
 home1.fan_off()
 home1.check()
+
+
+class Parent:
+
+    def __init__(self):
+        self.x = 10
+    
+    def add(self,x,y):
+        return x+y
+    
+class Childe(Parent):
+ 
+    def __init__(self):
+        self.x =20
+    
+    def add(self,*args):
+        return sum(args)
+    
+    def adding(self,x,y,z) -> int :
+        return x+y+z
+
+child  = Childe()
+
+print(child.add(3,3,3))
+print(child.adding(3,3,3))
+print(child.x)
+
+
+# Abstraction
+
+from abc import ABC,abstractmethod
+
+class Student(ABC):
+
+    @abstractmethod
+    def adding(self,x:int,y:int) -> int:
+        pass
+
+class StudentImp(Student):
+
+    def adding(self,x:int,y:int):
+        return x+y
+
+student = StudentImp()
+print(student.adding(3,3))
