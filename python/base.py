eed0a4533f2ab7a4a901433f8f450f32e6382506
Sony <86650412+Anishsony1999@users.noreply.github.com>
2025-08-27 14:20:07 +0530

    adding

---
+++ python/base.py
@@ -0,0 +1,17 @@
+
+x = 10
+
+if x > 5:
+    print("x is grater then 5")
+    if x == 10:
+        print(x)
+else:
+    print("")
+
+# 2000
+
+# 1 - 12 -> child
+# 13 - 19 -> teen
+# 20 - 40 -> adalt
+# 40 - 60 -> elders
+# 60 -> senior
---
+++ python/string.py
@@ -0,0 +1,11 @@
+
+
+# String - data type
+
+lname = 'Sony'
+fname = str("Ani")
+
+full_name = "Anish Sony"
+
+print(full_name[6])
+print(len(full_name))
